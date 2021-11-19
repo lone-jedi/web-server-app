9a2a924749d721980fe795f5e869592b165c5dd2
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-11-19 11:00:51 +0200

    Add ServerSocket initialize in try with resources block

--- src/main/java/com/yarkin/server/Server.java
+++ src/main/java/com/yarkin/server/Server.java
@@ -18,62 +18,64 @@ public class Server {
     }
 
     public void listen() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(port);
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
 
-        while (true) {
-            try (Socket socket = serverSocket.accept();
-                 BufferedReader reader = new BufferedReader(
-                         new InputStreamReader(socket.getInputStream()));
-                 BufferedWriter writer = new BufferedWriter(
-                         new OutputStreamWriter(socket.getOutputStream()));) {
-                // Obtain data from client
-                char[] inputBytes = new char[32 * 1024]; // 32KB
-                int countOfBytes = reader.read(inputBytes);
-                String requestText = countOfBytes >= 0 ? new String(inputBytes, 0, countOfBytes) : "";
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader reader = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()));
+                     BufferedWriter writer = new BufferedWriter(
+                             new OutputStreamWriter(socket.getOutputStream()));) {
+                    // Obtain data from client
+                    char[] inputBytes = new char[32 * 1024]; // 32KB
+                    int countOfBytes = reader.read(inputBytes);
+                    String requestText = countOfBytes >= 0 ? new String(inputBytes, 0, countOfBytes) : "";
 
-                // Parse client request
-                HttpRequest request = new HttpRequest(requestText);
-                HttpResponse response = new HttpResponse();
+                    // Parse client request
+                    HttpRequest request = new HttpRequest(requestText);
+                    HttpResponse response = new HttpResponse();
 
-                // check if request path contains required
-                String pathToResource = pathToWebApp + request.getQueryUri();
-                Resource resource = new Resource(pathToResource);
-                if (!new File(pathToResource).exists()) {
-                    response.setStatus(404);
-                    response.setContentType(ContentType.HTML);
-                    response.setContent("<h1>404 Not Found</h1>");
+                    // check if request path contains required
+                    String pathToResource = pathToWebApp + request.getQueryUri();
+                    Resource resource = new Resource(pathToResource);
+                    if (!new File(pathToResource).exists()) {
+                        response.setStatus(404);
+                        response.setContentType(ContentType.HTML);
+                        response.setContent("<h1>404 Not Found</h1>");
+                        writer.write(response.getHttp());
+                        continue;
+                    }
+
+                    // get file content
+                    byte[] content = new byte[1024 * 1024 * 4]; // 4 MB
+                    int bytesCount = resource.load(content);
+
+                    // write to outputStream
+                    ContentType contentType = request.getContentType();
+                    response.setStatus(200);
+                    response.setContentType(contentType);
+
+                    if (bytesCount <= 0) {
+                        writer.write(response.getHttp());
+                        continue;
+                    }
+
+                    if (contentType.equals(ContentType.PNG) || contentType.equals(ContentType.JPEG)) {
+                        writer.write(response.getHttp());
+                        writer.write("\n");
+                        socket.getOutputStream().write(content, 0, bytesCount);
+                        continue;
+                    }
+
+                    response.setContent(new String(content, 0, bytesCount));
                     writer.write(response.getHttp());
-                    continue;
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } catch (Throwable e) {
+                    e.printStackTrace();
                 }
-
-                // get file content
-                byte[] content = new byte[1024 * 1024 * 4]; // 4 MB
-                int bytesCount = resource.load(content);
-
-                // write to outputStream
-                ContentType contentType = request.getContentType();
-                response.setStatus(200);
-                response.setContentType(contentType);
-
-                if (bytesCount <= 0) {
-                    writer.write(response.getHttp());
-                    continue;
-                }
-
-                if (contentType.equals(ContentType.PNG) || contentType.equals(ContentType.JPEG)) {
-                    writer.write(response.getHttp());
-                    writer.write("\n");
-                    socket.getOutputStream().write(content, 0, bytesCount);
-                    continue;
-                }
-
-                response.setContent(new String(content, 0, bytesCount));
-                writer.write(response.getHttp());
-            } catch (IllegalStateException e) {
-                e.printStackTrace();
-            } catch (Throwable e) {
-                e.printStackTrace();
             }
+
         }
     }
 
